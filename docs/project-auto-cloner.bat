88f76dd2be2f0fbafc0a4dffeff1516a7cb527d3
GunaRakulan-GR <gunarakulan@gmail.com>
2023-09-09 13:44:41 +0530

    fix: update links

--- docs/project-auto-cloner.bat
+++ docs/project-auto-cloner.bat
@@ -4,3 +4,4 @@ git clone https://github.com/gunarakulangunaretnam/face-mesh-detection-with-cvzo
 git clone https://github.com/gunarakulangunaretnam/pose-detection-with-cvzone-python
 git clone https://github.com/gunarakulangunaretnam/hand-tracker-with-cvzone-python
 git clone https://github.com/gunarakulangunaretnam/face-detection-in-python
+git clone https://github.com/gunarakulangunaretnam/virtual-driving
