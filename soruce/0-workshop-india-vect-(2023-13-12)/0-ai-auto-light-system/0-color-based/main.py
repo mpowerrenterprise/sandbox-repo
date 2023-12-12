0bfb20cddaa4de50b854fc51ff99eb8cccf66ac7
GunaRakulan-GR <gunarakulan@gmail.com>
2023-12-12 17:35:05 +0530

    feat: build auto-light-system

---
+++ soruce/0-workshop-india-vect-(2023-13-12)/0-ai-auto-light-system/0-color-based/main.py
@@ -0,0 +1,53 @@
+import cv2
+import utils
+import numpy as np
+from pyfirmata import Arduino, util
+
+board = Arduino('COM4') # Change 'COM4' to your Arduino's serial port
+
+red_pin = board.get_pin('d:13:o')     # Digital output pin 13
+green_pin = board.get_pin('d:12:o')   # Digital output pin 12
+blue_pin = board.get_pin('d:11:o')    # Digital output pin 11
+
+# Open the camera
+cap = cv2.VideoCapture(2)
+
+while True:
+    # Read a frame from the camera
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Error reading frame")
+        break
+
+    # Get the dominant color in the frame
+    dominant_color = utils.get_dominant_color(frame)
+
+    # Draw the dominant color on the frame
+    cv2.putText(frame, f"Dominant Color: {dominant_color}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+
+    if dominant_color == "Red":
+        red_pin.write(1)
+
+    elif dominant_color == "Green":
+        green_pin.write(1)
+
+    elif dominant_color == "Blue":
+        blue_pin.write(1)
+    else:
+        red_pin.write(0)
+        green_pin.write(0)
+        blue_pin.write(0)
+
+
+    # Display the annotated frame
+    cv2.imshow('Dominant Color Detection', frame)
+
+    # Break the loop when 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close all windows
+cap.release()
+cv2.destroyAllWindows()
--- soruce/0-workshop-india-vect-(2023-13-12)/0-ai-auto-light-system/0-color-based/utils.py
+++ soruce/0-workshop-india-vect-(2023-13-12)/0-ai-auto-light-system/0-color-based/utils.py
@@ -1,23 +1,40 @@
-import speech_recognition as sr
+import cv2
+import numpy as np
 
-def speech_to_text():
-    # Initialize the recognizer
-    recognizer = sr.Recognizer()
+def get_dominant_color(frame, threshold=500):
+    # Convert the BGR image to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    try:
-        with sr.Microphone() as source:
-            print("Listening...")
-            audio_data = recognizer.listen(source, timeout=5)  # Listen to the microphone for 5 seconds
+    # Define color ranges in HSV
+    lower_red = np.array([0, 100, 100])
+    upper_red = np.array([10, 255, 255])
 
-        print("Recognizing...:", text)
-        text = recognizer.recognize_google(audio_data)
-        
-        return text
+    lower_green = np.array([40, 40, 40])
+    upper_green = np.array([80, 255, 255])
 
-    except sr.UnknownValueError:
-        print("Sorry, I could not understand the audio.")
-        return None
+    lower_blue = np.array([100, 100, 100])
+    upper_blue = np.array([140, 255, 255])
 
-    except sr.RequestError as e:
-        print(f"Error connecting to Google Web Speech API: {e}")
-        return None
+    # Threshold the image to get only specified colors
+    mask_red = cv2.inRange(hsv, lower_red, upper_red)
+    mask_green = cv2.inRange(hsv, lower_green, upper_green)
+    mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # Get the area of each color
+    red_area = cv2.countNonZero(mask_red)
+    green_area = cv2.countNonZero(mask_green)
+    blue_area = cv2.countNonZero(mask_blue)
+
+    # Determine the dominant color
+    dominant_color = "Unknown"
+    max_area = max(red_area, green_area, blue_area)
+    
+    if max_area > threshold:
+        if max_area == red_area:
+            dominant_color = "Red"
+        elif max_area == green_area:
+            dominant_color = "Green"
+        elif max_area == blue_area:
+            dominant_color = "Blue"
+
+    return dominant_color
